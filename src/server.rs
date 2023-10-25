1f611cc64627f6bc0ebbff4616a426077e857111
Ismael Pamplona <ismaelpamplona@hotmail.com>
2023-10-25 16:44:19 -0300

    (up) Issue #listen_fro_tcp_connections.

--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,8 @@
+use std::io::Read;
+use std::net::TcpListener;
+
 pub struct Server {
-    addr: String
+    addr: String,
 }
 
 impl Server {
@@ -9,5 +12,20 @@ impl Server {
 
     pub fn run(self) {
         println!("Listening on {}", self.addr);
+        let listener = TcpListener::bind(&self.addr).unwrap();
+        loop {
+            match listener.accept() {
+                Ok((mut stream, addr)) => {
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+                        }
+                        Err(e) => println!("Reading from the connection failed: {}", e),
+                    }
+                }
+                Err(e) => println!("The attempt to establish a connection has failed: {}", e),
+            }
+        }
     }
-} 
+}
